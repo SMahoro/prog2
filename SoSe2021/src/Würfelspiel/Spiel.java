1136dd704e03ba303f37fd9f457c4c0afab6128e
S.Mahoro <mahoro-nkundineza@web.de>
2021-04-21 22:03:06 +0200

    update Aufgabe 1

--- SoSe2021/src/Würfelspiel/Spiel.java
+++ SoSe2021/src/Würfelspiel/Spiel.java
@@ -60,4 +60,23 @@ public class Spiel {
 			name = JOptionPane.showInputDialog("Name des Spielers " + (i+1)+" :");
 			spieler[i] = new Spieler(name);
 		}
-	}}
+	}
+	
+	public void spielen()
+	{
+		int index = 0;
+		boolean gewonnen = false;
+		while(!gewonnen)
+		{
+			gewonnen = spieler[index].wuerfeln();
+			index = (index<spieler.length-1) ? index+1 : 0;
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		Spiel spiel = new Spiel();
+		spiel.spielen();
+	}
+	
+}
