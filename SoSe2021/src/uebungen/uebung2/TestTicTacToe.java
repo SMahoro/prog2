01da91006d78493f2950c65d9f13bb96d59d279d
S.Mahoro <mahoro-nkundineza@web.de>
2021-04-27 14:47:48 +0200

    update

--- SoSe2021/src/uebungen/uebung2/TestTicTacToe.java
+++ SoSe2021/src/uebungen/uebung2/TestTicTacToe.java
@@ -6,14 +6,34 @@ public class TestTicTacToe {
 TicTacToe ttt= new TicTacToe();
 ttt.print();
 ttt.makeMove(1, 1, State.RED);
+ttt.printResultat();
 ttt.print();
 ttt.makeMove(1, 2, State.BLACK);
+ttt.printResultat();
 ttt.print();
-ttt.makeMove(1, 1, State.BLACK);
+ttt.makeMove(1, -1, State.BLACK);
+ttt.printResultat();
 ttt.print();
-ttt.makeMove(0, 0, State.RED);
+ttt.makeMove(0, 1, State.RED);
+ttt.printResultat();
+ttt.print();
+ttt.makeMove(2, 1, State.BLACK);
+ttt.printResultat();
+ttt.print();
+ttt.makeMove(1, 0, State.RED);
+ttt.printResultat();
+ttt.print();
+ttt.makeMove(0, 0, State.BLACK);
+ttt.printResultat();
+ttt.print();
+ttt.makeMove(0, 2, State.RED);
+ttt.printResultat();
+ttt.print();
+ttt.makeMove(2, 0, State.BLACK);
+ttt.printResultat();
 ttt.print();
 ttt.makeMove(2, 2, State.RED);
+ttt.printResultat();
 ttt.print();
 
 
