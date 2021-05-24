061f629fb936bae19982ec979337a34999ed1932
S.Mahoro <mahoro-nkundineza@web.de>
2021-05-24 09:52:22 +0200

    Aufgabe5

---
+++ SoSe2021/src/Aufgabe5/Test.java
@@ -0,0 +1,17 @@
+package Aufgabe5;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+			SetOperations so = new SetOperations();
+			so.fill();
+			so.fillBothUnion();
+			so.print();
+			so.fillBothIntersection();
+			so.print();
+			so.fillBothDifference();
+			so.print();
+		}
+
+	}
