dfff956d9b2f8269a92f8ddef54a761b8296be26
S.Mahoro <mahoro-nkundineza@web.de>
2021-06-13 11:10:54 +0200

    Aufgabe7

---
+++ SoSe2021/src/Aufgabe7/Listener.java
@@ -0,0 +1,8 @@
+package Aufgabe7;
+
+public interface Listener {
+	
+	public void update();
+	public void setPublisher(Publisher publisher);
+	public void removePublisher(Publisher publisher);
+}
---
+++ SoSe2021/src/Aufgabe7/Publisher.java
@@ -0,0 +1,9 @@
+package Aufgabe7;
+
+public interface Publisher {
+	
+	public boolean register(Listener listener);
+	public boolean unregister(Listener listener);
+	public void notifyListeners();
+	public String getUpdate(Listener listener);
+}
